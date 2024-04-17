9340c5974ed7fe2f51688054dadd6fef882b07b8
laibaiqbal552 <laibaiqbal552@gmail.com>
2024-04-17 07:28:06 +0500

    .

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -27,10 +27,7 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <meta
-          property="og:image"
-          content="https://paradise-dev-weld.vercel.app/images/WALL-PARADISEDEV-DARK.png"
-        />
+        <meta property="og:image" content="/images/WALL-PARADISEDEV-DARK.png" />
         {/* Add other meta tags as needed */}
       </head>
       <body
