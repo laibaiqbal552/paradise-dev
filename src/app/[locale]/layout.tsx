614a02ad2ada66945ff9f64384551693a38518bc
laibaiqbal552 <laibaiqbal552@gmail.com>
2024-04-17 07:20:23 +0500

    .

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="icon" href="./../favicon.ico" />
+        <link rel="icon" href="/images/WALL-PARADISEDEV- DARK.png" />
         {/* Add other meta tags as needed */}
       </head>
       <body
