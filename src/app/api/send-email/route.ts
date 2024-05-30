ef3ed7cacfa60b9a80c755340a6747097a11b8f9
fraz khan <fraz9838@gmail.com>
2024-05-30 21:02:17 +0500

    send grid implementation

---
+++ src/app/api/send-email/route.ts
@@ -0,0 +1,29 @@
+import sgMail from "@sendgrid/mail";
+import { NextResponse, NextRequest } from "next/server";
+
+sgMail.setApiKey(process.env.NEXT_SENDGRID_KEY as string);
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const { name, email, affair, message } = data;
+
+    const msg = {
+      to: email,
+      from: "no-reply@paradisegaming.net",
+      subject: `New message from ${email}`,
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Affair: ${affair}
+        Message: ${message}
+      `,
+    };
+
+    await sgMail.send(msg);
+    return NextResponse.json({ success: true, data });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return NextResponse.json({ success: false, error: error });
+  }
+}
